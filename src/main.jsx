5740f4446ed8d0b533ba23258808047b547062ca
Kang Cahya <cahyadyazin@yahoo.com>
2026-09-21 13:29:23 +0700

    Add analitycs

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Analytics } from "@vercel/analytics/react";
 import App from "./App.jsx";
 import "./index.css";
 import { LanguageProvider } from "./hooks/useLanguage.jsx";
@@ -9,6 +10,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <LanguageProvider translations={translations}>
       <App />
+      <Analytics />
     </LanguageProvider>
   </React.StrictMode>,
 );
